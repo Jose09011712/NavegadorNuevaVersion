23b6b8bbc559b294015878489e55ec6c2ef880de
Jose09011712 <jlopezt11@miumg.edu.gt>
2021-10-07 14:53:56 -0600

    Navegador nueva Version 1.2

---
+++ SeguridadNavegador/Navegador/CapaVista/Navegador.Designer.cs
@@ -0,0 +1,284 @@
+﻿namespace CapaVistaNavegador
+{
+    partial class Navegador
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Navegador));
+            this.gbxNavegador = new System.Windows.Forms.GroupBox();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.btnAyuda = new System.Windows.Forms.Button();
+            this.btnInicio = new System.Windows.Forms.Button();
+            this.btnAdelante = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnAtras = new System.Windows.Forms.Button();
+            this.btnFinal = new System.Windows.Forms.Button();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnRefrescar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnInsertar = new System.Windows.Forms.Button();
+            this.ttAyuda = new System.Windows.Forms.ToolTip(this.components);
+            this.gbxNavegador.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gbxNavegador
+            // 
+            this.gbxNavegador.Controls.Add(this.btnSalir);
+            this.gbxNavegador.Controls.Add(this.btnAyuda);
+            this.gbxNavegador.Controls.Add(this.btnInicio);
+            this.gbxNavegador.Controls.Add(this.btnAdelante);
+            this.gbxNavegador.Controls.Add(this.btnGuardar);
+            this.gbxNavegador.Controls.Add(this.btnAtras);
+            this.gbxNavegador.Controls.Add(this.btnFinal);
+            this.gbxNavegador.Controls.Add(this.btnModificar);
+            this.gbxNavegador.Controls.Add(this.btnCancelar);
+            this.gbxNavegador.Controls.Add(this.btnRefrescar);
+            this.gbxNavegador.Controls.Add(this.btnEliminar);
+            this.gbxNavegador.Controls.Add(this.btnInsertar);
+            this.gbxNavegador.Font = new System.Drawing.Font("Times New Roman", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gbxNavegador.Location = new System.Drawing.Point(8, 4);
+            this.gbxNavegador.Margin = new System.Windows.Forms.Padding(4);
+            this.gbxNavegador.Name = "gbxNavegador";
+            this.gbxNavegador.Padding = new System.Windows.Forms.Padding(4);
+            this.gbxNavegador.Size = new System.Drawing.Size(995, 70);
+            this.gbxNavegador.TabIndex = 0;
+            this.gbxNavegador.TabStop = false;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.BackColor = System.Drawing.Color.White;
+            this.btnSalir.Enabled = false;
+            this.btnSalir.Font = new System.Drawing.Font("Rockwell", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalir.Image = ((System.Drawing.Image)(resources.GetObject("btnSalir.Image")));
+            this.btnSalir.Location = new System.Drawing.Point(910, 11);
+            this.btnSalir.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(74, 50);
+            this.btnSalir.TabIndex = 13;
+            this.btnSalir.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnSalir.UseVisualStyleBackColor = false;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // btnAyuda
+            // 
+            this.btnAyuda.BackColor = System.Drawing.Color.White;
+            this.btnAyuda.Enabled = false;
+            this.btnAyuda.Font = new System.Drawing.Font("Rockwell", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAyuda.Image = ((System.Drawing.Image)(resources.GetObject("btnAyuda.Image")));
+            this.btnAyuda.Location = new System.Drawing.Point(828, 11);
+            this.btnAyuda.Margin = new System.Windows.Forms.Padding(4);
+            this.btnAyuda.Name = "btnAyuda";
+            this.btnAyuda.Size = new System.Drawing.Size(74, 50);
+            this.btnAyuda.TabIndex = 12;
+            this.btnAyuda.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnAyuda.UseVisualStyleBackColor = false;
+            this.btnAyuda.Click += new System.EventHandler(this.btnAyuda_Click);
+            // 
+            // btnInicio
+            // 
+            this.btnInicio.BackColor = System.Drawing.Color.White;
+            this.btnInicio.Enabled = false;
+            this.btnInicio.Font = new System.Drawing.Font("Rockwell", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInicio.Image = ((System.Drawing.Image)(resources.GetObject("btnInicio.Image")));
+            this.btnInicio.Location = new System.Drawing.Point(500, 11);
+            this.btnInicio.Margin = new System.Windows.Forms.Padding(4);
+            this.btnInicio.Name = "btnInicio";
+            this.btnInicio.Size = new System.Drawing.Size(74, 50);
+            this.btnInicio.TabIndex = 11;
+            this.btnInicio.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnInicio.UseVisualStyleBackColor = false;
+            this.btnInicio.Click += new System.EventHandler(this.btnInicio_Click);
+            // 
+            // btnAdelante
+            // 
+            this.btnAdelante.BackColor = System.Drawing.Color.White;
+            this.btnAdelante.Enabled = false;
+            this.btnAdelante.Font = new System.Drawing.Font("Rockwell", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdelante.Image = ((System.Drawing.Image)(resources.GetObject("btnAdelante.Image")));
+            this.btnAdelante.Location = new System.Drawing.Point(664, 11);
+            this.btnAdelante.Margin = new System.Windows.Forms.Padding(4);
+            this.btnAdelante.Name = "btnAdelante";
+            this.btnAdelante.Size = new System.Drawing.Size(74, 50);
+            this.btnAdelante.TabIndex = 8;
+            this.btnAdelante.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnAdelante.UseVisualStyleBackColor = false;
+            this.btnAdelante.Click += new System.EventHandler(this.btnAdelante_Click);
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.BackColor = System.Drawing.Color.White;
+            this.btnGuardar.Enabled = false;
+            this.btnGuardar.Font = new System.Drawing.Font("Rockwell", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuardar.Image = ((System.Drawing.Image)(resources.GetObject("btnGuardar.Image")));
+            this.btnGuardar.Location = new System.Drawing.Point(336, 11);
+            this.btnGuardar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(74, 50);
+            this.btnGuardar.TabIndex = 1;
+            this.btnGuardar.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnGuardar.UseVisualStyleBackColor = false;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // btnAtras
+            // 
+            this.btnAtras.BackColor = System.Drawing.Color.White;
+            this.btnAtras.Enabled = false;
+            this.btnAtras.Font = new System.Drawing.Font("Rockwell", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAtras.Image = ((System.Drawing.Image)(resources.GetObject("btnAtras.Image")));
+            this.btnAtras.Location = new System.Drawing.Point(582, 11);
+            this.btnAtras.Margin = new System.Windows.Forms.Padding(4);
+            this.btnAtras.Name = "btnAtras";
+            this.btnAtras.Size = new System.Drawing.Size(74, 50);
+            this.btnAtras.TabIndex = 10;
+            this.btnAtras.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnAtras.UseVisualStyleBackColor = false;
+            this.btnAtras.Click += new System.EventHandler(this.btnAtras_Click);
+            // 
+            // btnFinal
+            // 
+            this.btnFinal.BackColor = System.Drawing.Color.White;
+            this.btnFinal.Enabled = false;
+            this.btnFinal.Font = new System.Drawing.Font("Rockwell", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFinal.Image = ((System.Drawing.Image)(resources.GetObject("btnFinal.Image")));
+            this.btnFinal.Location = new System.Drawing.Point(746, 11);
+            this.btnFinal.Margin = new System.Windows.Forms.Padding(4);
+            this.btnFinal.Name = "btnFinal";
+            this.btnFinal.Size = new System.Drawing.Size(74, 50);
+            this.btnFinal.TabIndex = 9;
+            this.btnFinal.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnFinal.UseVisualStyleBackColor = false;
+            this.btnFinal.Click += new System.EventHandler(this.btnFinal_Click);
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.BackColor = System.Drawing.Color.White;
+            this.btnModificar.Enabled = false;
+            this.btnModificar.Font = new System.Drawing.Font("Rockwell", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnModificar.Image = global::CapaVistaNavegador.Properties.Resources.Modificar_;
+            this.btnModificar.Location = new System.Drawing.Point(90, 11);
+            this.btnModificar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(74, 50);
+            this.btnModificar.TabIndex = 7;
+            this.btnModificar.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnModificar.UseVisualStyleBackColor = false;
+            this.btnModificar.Click += new System.EventHandler(this.btnModificar_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.BackColor = System.Drawing.Color.White;
+            this.btnCancelar.Enabled = false;
+            this.btnCancelar.Font = new System.Drawing.Font("Rockwell", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelar.Image = ((System.Drawing.Image)(resources.GetObject("btnCancelar.Image")));
+            this.btnCancelar.Location = new System.Drawing.Point(418, 11);
+            this.btnCancelar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(74, 50);
+            this.btnCancelar.TabIndex = 6;
+            this.btnCancelar.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnCancelar.UseVisualStyleBackColor = false;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // btnRefrescar
+            // 
+            this.btnRefrescar.BackColor = System.Drawing.Color.White;
+            this.btnRefrescar.Enabled = false;
+            this.btnRefrescar.Font = new System.Drawing.Font("Rockwell", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRefrescar.Image = ((System.Drawing.Image)(resources.GetObject("btnRefrescar.Image")));
+            this.btnRefrescar.Location = new System.Drawing.Point(254, 11);
+            this.btnRefrescar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnRefrescar.Name = "btnRefrescar";
+            this.btnRefrescar.Size = new System.Drawing.Size(74, 50);
+            this.btnRefrescar.TabIndex = 3;
+            this.btnRefrescar.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnRefrescar.UseVisualStyleBackColor = false;
+            this.btnRefrescar.Click += new System.EventHandler(this.btnRefrescar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.BackColor = System.Drawing.Color.White;
+            this.btnEliminar.Enabled = false;
+            this.btnEliminar.Font = new System.Drawing.Font("Rockwell", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEliminar.Image = ((System.Drawing.Image)(resources.GetObject("btnEliminar.Image")));
+            this.btnEliminar.Location = new System.Drawing.Point(172, 11);
+            this.btnEliminar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(74, 50);
+            this.btnEliminar.TabIndex = 2;
+            this.btnEliminar.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnEliminar.UseVisualStyleBackColor = false;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnInsertar
+            // 
+            this.btnInsertar.BackColor = System.Drawing.Color.White;
+            this.btnInsertar.Enabled = false;
+            this.btnInsertar.Font = new System.Drawing.Font("Rockwell", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInsertar.Image = global::CapaVistaNavegador.Properties.Resources.Insertar;
+            this.btnInsertar.Location = new System.Drawing.Point(8, 11);
+            this.btnInsertar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnInsertar.Name = "btnInsertar";
+            this.btnInsertar.Size = new System.Drawing.Size(74, 50);
+            this.btnInsertar.TabIndex = 0;
+            this.btnInsertar.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnInsertar.UseVisualStyleBackColor = false;
+            this.btnInsertar.Click += new System.EventHandler(this.btnInsertar_Click);
+            // 
+            // Navegador
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Transparent;
+            this.Controls.Add(this.gbxNavegador);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Navegador";
+            this.Size = new System.Drawing.Size(1007, 79);
+            this.gbxNavegador.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnInsertar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnRefrescar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.Button btnFinal;
+        private System.Windows.Forms.Button btnAtras;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnAdelante;
+        private System.Windows.Forms.Button btnInicio;
+        private System.Windows.Forms.Button btnAyuda;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.GroupBox gbxNavegador;
+        private System.Windows.Forms.ToolTip ttAyuda;
+    }
+}
